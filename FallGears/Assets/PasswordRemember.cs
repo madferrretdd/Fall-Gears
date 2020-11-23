e5546c75386c106d362e20b737addb2ac7a76834
madferrretdd <43165553+madferrretdd@users.noreply.github.com>
2020-11-23 15:56:14 +0000

    Added Auth front screen
    still need to unbreak all scene refs that refer to numbers

---
+++ FallGears/Assets/PasswordRemember.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordRemember : MonoBehaviour
+{
+    const string playerNamePrefKey = "Password";
+    public int BoxTickedInt;
+
+    public Toggle toggleBox;
+    public AuthManager AuthMan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string defaultName = string.Empty;
+        InputField _inputField = this.GetComponent<InputField>();
+
+        BoxTickedInt = PlayerPrefs.GetInt("BoxTicked");
+
+            if (PlayerPrefs.GetInt("BoxTicked") == 1 && PlayerPrefs.HasKey(playerNamePrefKey))
+                {
+                    toggleBox.isOn = true;
+                    defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                    _inputField.text = defaultName;
+                    PlayerPrefs.SetInt("BoxTicked", 1);
+            }
+            else if (PlayerPrefs.GetInt("BoxTicked") == 0)
+            {
+                toggleBox.isOn = false;
+            }
+    }
+
+
+    public void Ticked()
+    {
+        if (PlayerPrefs.GetInt("BoxTicked") == 1)
+        {
+            PlayerPrefs.SetInt("BoxTicked", 0);
+        }
+        else if (PlayerPrefs.GetInt("BoxTicked") == 0)
+        {
+            PlayerPrefs.SetInt("BoxTicked", 1);
+        }
+    }
+
+    public void SetPassword(string value)
+    {
+        // #Important
+        if (string.IsNullOrEmpty(value))
+        {
+            Debug.LogError("Password is null or empty");
+            return;
+        }
+
+            PlayerPrefs.SetString(playerNamePrefKey, value);
+            PlayerPrefs.SetInt("BoxTicked", BoxTickedInt);
+
+    }
+}
---
+++ FallGears/Assets/SaveEmail.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveEmail : MonoBehaviour
+{
+
+    const string playerNamePrefKey = "EmailAddress";
+    // Start is called before the first frame update
+    void Start()
+    {
+        string defaultName = string.Empty;
+        InputField _inputField = this.GetComponent<InputField>();
+
+        if (_inputField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                _inputField.text = defaultName;
+            }
+        }
+    }
+
+    public void SetEmailName(string value)
+    {
+        // #Important
+        if (string.IsNullOrEmpty(value))
+        {
+            Debug.LogError("Player Name is null or empty");
+            return;
+        }
+
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+}
